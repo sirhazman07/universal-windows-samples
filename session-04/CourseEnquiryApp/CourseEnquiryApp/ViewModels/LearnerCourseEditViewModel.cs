32371cb2b47e3d89ad1886badcdba3ace9fb84ee
Harold Baldwin <haroldbaldwin@hotmail.com>
2016-03-07 12:00:04 +1100

    added missing files for edit View

---
+++ session-04/CourseEnquiryApp/CourseEnquiryApp/ViewModels/LearnerCourseEditViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseEnquiryApp.ViewModels
+{
+    class LearnerCourseEditViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(ref _name, value); }
+        }
+
+        private string _summary;
+
+        public string Summary
+        {
+            get { return _summary; }
+            set { Set(ref _summary, value); }
+        }
+
+        private string _overview;
+
+        public string Overview
+        {
+            get { return _overview; }
+            set { Set(ref _overview, value); }
+        }
+        private DateTime _startDate;
+
+        public DateTime Startdate
+        {
+            get { return _startDate; }
+            set { Set(ref _startDate, value); }
+        }
+        private DateTime _endDate;
+
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { Set(ref _endDate, value); }
+        }
+    }
+}
---
+++ session-04/CourseEnquiryApp/CourseEnquiryApp/Views/LearnerCourseEdit.xaml.cs
@@ -0,0 +1,42 @@
+﻿using CourseEnquiryApp.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Data.Entity;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CourseEnquiryApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LearnerCourseEdit : Page
+    {
+        public LearnerCourseEdit()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sebder, RoutedEventArgs e)
+        {
+            using (var context = new CourseEnquiryDbContext())
+            {
+                // need to recheck logic 
+                var LearnerCourse = LearnerCourse.Get(LearnerId)
+            }
+        }
+    }
+}
